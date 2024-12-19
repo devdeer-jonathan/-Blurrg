ad1f0ede4e4e3993b33b87da2e19aa05c0ddb16f
Jonathan Lenz <jonathan.lenz@devdeer.com>
2024-12-19 13:37:31 +0100

    implemented logic to get basic information about the current OS

---
+++ src/Logic/Logic.Core/OperatingSystemLogic.cs
@@ -0,0 +1,31 @@
+﻿namespace Logic.Core
+{
+    using System.Runtime.InteropServices;
+
+    /// <summary>
+    /// Contains logic related to operating systems.
+    /// </summary>
+    public class OperatingSystemLogic
+    {
+        #region methods
+
+        /// <summary>
+        /// Detects and returns the current runtime operating system platform.
+        /// </summary>
+        /// <returns>
+        /// The detected <see cref="System.Runtime.InteropServices.OSPlatform" /> for the current runtime.
+        /// </returns>
+        /// <exception cref="ApplicationException">
+        /// Thrown when the operating system cannot be determined or is not supported.
+        /// </exception>
+        public OSPlatform GetRuntimeOperatingSystem()
+        {
+            return RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? OSPlatform.Windows :
+                RuntimeInformation.IsOSPlatform(OSPlatform.Linux) ? OSPlatform.Linux :
+                RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ? OSPlatform.OSX :
+                throw new ApplicationException($"Unsupported Operating System: {RuntimeInformation.RuntimeIdentifier}");
+        }
+
+        #endregion
+    }
+}
---
+++ src/Logic/Logic.Interfaces/Logic/IOperatingSystemLogic.cs
@@ -0,0 +1,22 @@
+﻿namespace Logic.Interfaces.Logic
+{
+    using System.Runtime.InteropServices;
+
+    public interface IOperatingSystemLogic
+    {
+        #region methods
+
+        /// <summary>
+        /// Detects and returns the current runtime operating system platform.
+        /// </summary>
+        /// <returns>
+        /// The detected <see cref="System.Runtime.InteropServices.OSPlatform" /> for the current runtime.
+        /// </returns>
+        /// <exception cref="ApplicationException">
+        /// Thrown when the operating system cannot be determined or is not supported.
+        /// </exception>
+        OSPlatform GetRuntimeOperatingSystem();
+
+        #endregion
+    }
+}
